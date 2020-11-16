5d2c524ba5e1a6b50c8e4f93fbb1e0accd4c4e9f
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 12:18:16 -0800

    added a welcome message to returning users

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom'
 const Nav = ({ authenticated, currentUser, className}) => {
   return( authenticated && currentUser ? (
     <header className={className}>
+      <div className="icon">Welcome Back {currentUser.name}</div>
       <nav>
         <div class="nav-wrapper">
         
           <Link  exact to='/' className="brand-logo">Home</Link >
             <ul className="right hide-on-med-and-down">
-              <li><Link to="/profile" className="nav-active btn" > Profile</Link></li>
+            <li><Link to="/Profile" className="nav-active btn" > Profile</Link></li>
               <li><Link to="/AllGossip" className="nav-active btn">Gossip <i className="material-icons right">cloud</i></Link ></li>
               <li><Link to="/upload" className="nav-active btn-large">Create Post</Link ></li>
               <li><Link to="/" onClick={() => localStorage.clear()} className="nav-active">Sign Out</Link></li>
