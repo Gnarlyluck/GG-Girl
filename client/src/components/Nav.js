189f08bf7ab9912f14321959fa70378a777a5a4d
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 07:45:25 -0800

    got allgossip to display post and gossip to display.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,7 +10,7 @@ const Nav = ({ authenticated, currentUser, className}) => {
           <Link  exact to='/' className="brand-logo">Home</Link >
             <ul className="right hide-on-med-and-down">
               <li><Link to="/profile" className="nav-active btn" > Profile</Link></li>
-              <li><Link to="/posts" className="nav-active btn">Posts <i className="material-icons right">cloud</i></Link ></li>
+              <li><Link to="/AllGossip" className="nav-active btn">Gossip <i className="material-icons right">cloud</i></Link ></li>
               <li><Link to="/upload" className="nav-active btn-large">Create Post</Link ></li>
               <li><Link to="/" onClick={() => localStorage.clear()} className="nav-active">Sign Out</Link></li>
             </ul>
