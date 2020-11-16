71fa7d3d9bbf00a0b617b7738b85019f665a1040
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 12:15:41 -0800

    created a page for users to submit posts

---
+++ client/src/pages/UploadPage.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import TextInput from '../components/TextInput'
+import { __UploadPost } from '../services/PostServices'
+
+class UploadPage extends Component {
+   constructor() {
+       super()
+       this.state = {
+           title: '',
+           location:'',
+           image_url: '',
+           description: ''
+       }
+   }
+   
+   handleChange = ({ target }) => {
+       this.setState({ [target.name]: target.value })
+   }
+
+   handleSubmit = async (event) => {
+       event.preventDefault()
+       try{
+        await __UploadPost(this.state, this.props.currentUser._id)
+        this.props.history.push('profile')
+       }catch (error) {
+           throw error
+       }
+   }
+
+    render() {
+        const{ title, location, image_url, description } = this.state
+       return (
+           <div>
+               <form onSubmit={this.handleSubmit}>
+                   <TextInput 
+                   name="title"
+                   value={title}
+                   onChange={this.handleChange}
+                   placeholder="Title"
+                   />
+                    <TextInput 
+                   name="location"
+                   value={location}
+                   onChange={this.handleChange}
+                   placeholder="Location"
+                   /> 
+                   <TextInput 
+                   name="image_url"
+                   value={image_url}
+                   onChange={this.handleChange}
+                   placeholder="Upload your Image URL here "
+                   />
+                    <TextInput 
+                   name="description"
+                   value={description}
+                   onChange={this.handleChange}
+                   placeholder="Description"
+                   fieldType="textfield"
+                   />
+                   <button>Upload</button>
+               </form>
+           </div>
+       )
+   }
+}
+
+export default UploadPage
