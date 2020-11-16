c7a577db0cfc07bb85b1ac2fb147f652a107b7d4
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 07:46:01 -0800

    got this to work!!

--- client/src/pages/AllGossip.js
+++ client/src/pages/AllGossip.js
@@ -4,53 +4,74 @@ import { __GetPosts} from '../services/PostServices'
 
 
 class AllGossip extends Component {
-    constructor(props) {
-        super(props)
+    constructor() {
+        super()
         this.state = {
           posts: [],
-         
+          currentPage: 1
         }
-    }
-
-    componentDidMount() {
-        this.getPosts()
-        console.log(this.getPosts())
-    }
-
-    getPosts = async () => {
-        const showPosts = await __GetPosts(this.state)
-        console.log(showPosts)
-        this.setState({ posts: [...this.state.posts, ...showPosts]})
-        console.log(this.setState)
-        console.log(showPosts)
-        }
+      }
     
-
-
-   render() {
-    const {posts} = this.state
-    console.log(posts)
-       return(
-       <div>
-           <Nav />
-            <h2> Let's Gossip </h2>
-        {posts.length ? (
-            posts.map((post) => (
-                <div>
-                    <h3>{post.title}</h3>
-            <p>{post.image_url}</p>
-            <p>{post.popularity_rating}</p>
-                </div>
-            ))
-        ) : (
-            <h3>fuck off </h3>
-        )}
-       </div>   
-       
-    )
-
-  }
-}
-
-
+      componentDidMount() {
+        this.getPosts()
+      }
+    
+      getPosts = async () => {
+        try {
+          const posts = await __GetPosts(this.state.currentPage)
+          this.setState({ posts: [...this.state.posts, ...posts] })
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    
+      incrementPage = () =>
+        this.setState(
+          (prevstate) => ({ currentPage: prevstate.currentPage + 1 }),
+          () => this.getPosts()
+        )
+    
+      render() {
+        const { posts } = this.state
+        return (
+          <div >
+              <Nav />
+            <h2>Popular Posts</h2>
+            <section >
+              {posts.length ? (
+                posts.map((post) => (
+                  <div
+                    key={post._id}
+                    onClick={() => this.props.history.push(`/Gossip/${post._id}`)}>
+                    <div >
+                      <div >
+                        <div >
+                          <h3>{post.title}</h3>
+                          <p>{post.description.substring(0, 550)}...</p>
+                        </div>
+                        <div >
+                          <div >
+                            <p>Comments</p>
+                            <p>{post.comments.length}</p>
+                          </div>
+                          <div >
+                            <p>Likes</p>
+                            <p>{post.popularity_rating}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <img src={post.image_url} alt="post"/>
+                  </div>
+                ))
+              ) : (
+                <h3>No Posts</h3>
+              )}
+              <button onClick={this.incrementPage}>Load More</button>
+            </section>
+          </div>
+        )
+      }
+    }
+    
 export default AllGossip
