f01b43c7a85d7171418732b7ae398a1204b8e42e
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 15:59:15 -0800

    added some icons

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,9 +10,9 @@ const Nav = ({ authenticated, currentUser, className}) => {
         
           <Link  exact to='/' className="brand-logo">Home</Link >
             <ul className="right hide-on-med-and-down">
-            <li><Link to="/profile" className="nav-active btn pink darken-4" > Profile</Link></li>
+            <li><Link to="/profile" className="nav-active btn pink darken-4" > Profile<i className="material-icons right">local_bar</i></Link></li>
               <li><Link to="/AllGossip" className="nav-active btn pink darken-4">Gossip <i className="material-icons right">whatshot</i></Link ></li>
-              <li><Link to="/upload" className="nav-active btn pink darken-4">Create Post</Link ></li>
+              <li><Link to="/upload" className="nav-active btn pink darken-4">Create Post<i className="material-icons right">perm_media</i></Link ></li>
               <li><Link to="/" onClick={() => localStorage.clear()} className="nav-active">Sign Out</Link></li>
             </ul>
         </div>
@@ -22,12 +22,12 @@ const Nav = ({ authenticated, currentUser, className}) => {
     <header className={className}>
       <div className="icon"></div>
         <nav>
-          <div className="nav-wrapper">
+          <div className="nav-wrapper black">
             <Link exact to='/' className="brand-logo">Home</Link>
               <ul className="right hide-on-med-and-down">
-                <li> <Link className="nav-active btn" to="/AllGossip">Gossip</Link></li>
-                <li> <Link className="nav-active btn" to="/SignUp">Sign Up<i className="material-icons right">cloud</i></Link> </li>
-                <li><Link className="nav-active btn" to="/SignIn">Sign In</Link></li>
+                <li> <Link className="nav-active btn pink darken-4" to="/AllGossip">Gossip<i className="material-icons right">whatshot</i></Link></li>
+                <li> <Link className="nav-active btn pink darken-4" to="/SignUp">Sign Up<i className="material-icons right">person_add  </i></Link> </li>
+                <li><Link className="nav-active btn pink darken-4" to="/SignIn">Sign In<i className="material-icons right">create</i></Link></li>
               </ul>
         </div>
       </nav>
