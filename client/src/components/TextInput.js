2c90177ff860a272dc6a8d89e20abeec1d046d44
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-17 13:43:10 -0800

    changed text input export to beginning of declaration. warning came with, but no error.

--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -1,23 +1,44 @@
 import React from 'react'
 
-const TextInput = (props) => 
-props.fieldType === 'textfield' ? (
-  <textarea 
-    type={props.type}
-    name={props.name}
-    value={props.value}
-    onChange={(e) => props.onChange(e)}
-    placeholder={props.placeholder}
-  />
-) : (
-  <input 
-  type={props.type}
-  name={props.name}
-  value={props.value}
-  onChange={(e) => props.onChange(e)}
-  placeholder={props.placeholder}
-  autoComplete="false"
-  />
-)
+export default (props) =>
+  props.fieldType === 'textfield' ? (
+    <textarea
+      type={props.type}
+      name={props.name}
+      value={props.value}
+      onChange={(e) => props.onChange(e)}
+      placeholder={props.placeholder}
+    />
+  ) : (
+    <input
+      type={props.type}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+      placeholder={props.placeholder}
+      autoComplete="false"
+    />
+  )
 
-export default TextInput
+
+// const TextInput = (props) => 
+// props.fieldType === 'textfield' ? (
+//   <textarea 
+//     type={props.type}
+//     name={props.name}
+//     value={props.value}
+//     onChange={(e) => props.onChange(e)}
+//     placeholder={props.placeholder}
+//   />
+// ) : (
+//   <input 
+//   type={props.type}
+//   name={props.name}
+//   value={props.value}
+//   onChange={(e) => props.onChange(e)}
+//   placeholder={props.placeholder}
+//   autoComplete="false"
+//   />
+// )
+
+// export default TextInput
