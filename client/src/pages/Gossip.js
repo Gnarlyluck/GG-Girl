d12c1b4f89c0e0d20f6eb6489d7280213921deec
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-17 13:39:44 -0800

    got comments to dislpay and store in db

--- client/src/pages/Gossip.js
+++ client/src/pages/Gossip.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { __GetPost } from '../services/PostServices'
+import TextInput from '../components/TextInput'
+import {__PostComment, __ShowComments } from '../services/CommentService'
+// import { index } from '../../../db/models/GG-user'
 
 
 export default class Gossip extends Component {
     constructor() {
       super()
       this.state = {
-        post: null
+        post: null,
+        comment: '',
+        comments: []
       }
     }
 
     componentDidMount() {
         this.getPost()
       }
-    
+
+      handleChange = ({ target }) => {
+        this.setState({ [target.name]: target.value })
+    }
+
+    handleSubmit = async (event) => {
+      event.preventDefault()
+      try{
+          console.log(this.props)
+          await __PostComment (this.state,  
+            this.props.currentUser._id, 
+            this.props.match.params.post_id)
+
+          this.props.history.push('comments')
+      }catch (error){
+          throw error
+      }
+  }
+ 
+    showComments = async () => {
+      try{
+        const comments = await __ShowComments(this.props.match.params.post_id)
+        this.setState({comments: comments})
+      }catch (error) {
+        throw error
+      }
+    }
+
       getPost = async () => {
-        console.log('hello')
         try {
           const post = await __GetPost(this.props.match.params.post_id)
           this.setState({ post })
@@ -26,10 +57,10 @@ export default class Gossip extends Component {
       }
 
       render(){
-        const { post } = this.state
+        const { post, comment } = this.state
         if (this.state.post) {
           return (
-            <div style={{backgroundImage: 'url https://www.photocircle.net/eu/photos/thumbnails/zoom/80217-Manhattan-skyline-at-night--by-jan-becke.jpg'}}>
+            <div className="Gossip">
               <div className="row"> 
                 <div className="col s6  ">
                   <div className="card">
@@ -40,23 +71,39 @@ export default class Gossip extends Component {
                     <div className="card-content">
                       <p>{post.description}</p>
                     </div>
-                      <div>
-                        <p>Comments</p>
-                        <p>{post.comments.length}</p>
+                      <div className="card-action">
+                        <p onClick={this.showComments} >Comments </p>
+                          <form onSubmit={this.handleSubmit}>
+                            <TextInput 
+                              name="comment"
+                              value={comment}
+                              onChange={this.handleChange}
+                              placeholder="Write a comment"
+                              type="text"
+                            />
+                          </form>
+                        {this.state.comments.map((comment, index) => (
+                              <h3 key={index}>
+                                {
+                                comment.comment}
+                              </h3>
+                        ))}
                         <p>Likes</p>
                         <p>{post.popularity_rating}</p>
                       </div>
                   </div>
+                  <div>
                     {post.comments.length ? (
-                      post.comments.map((comment) => (
-                        <li className="comment-item" key={comment._id}>
-                          <p><Link>{comment.user_id.name}</Link></p>
+                      post.comments.map((comment, index) => (
+                        <li className="comment-item" key={index}>
+                          <p><Link>{comment.user_id}</Link></p>
                           <p>{comment.comment}</p>
                         </li>
                       ))
                       ) : (
                         <h3>No Comments</h3>
-                      )}
+                        )}
+                        </div>
                   </div>
               </div>
             </div>
