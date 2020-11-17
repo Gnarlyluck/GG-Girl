c882351d42cc8235ad38aa6287d1300139027152
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 22:01:51 -0800

    css thangs

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import GossipImg from '../img-file/gg-background.jpg'
-
+import '../styles/App.css'
 
 // style={{backgroundImage: `url(${GossipImg})`, backgroundSize: "cover", minHeight: "100vh"}}
 
 const Home = (props) => {
   return (
-  <div>
-        <p>XOXO GOSSIP GIRL</p>
+  <div className='mainBackground'>
+        <div className="GossipText">
+          <p className="welcome">Welcome</p> 
+        <p className="datTag">
+          to Gossip Girl
+          <br/>
+          The site ABOUT the in people
+          <br/>
+          FOR the in people
+          <br/>
+          By the in people
+          </p>
+        </div>
         <div className="img-wrapper">
         {/* <img src={GossipImg} alt="NY skyline with XOXO" /> */}
-        < div style={{backgroundImage: `url(${GossipImg})`, backgroundSize: "center", minHeight: "100vh"}} ></div>
+        {/* style={{backgroundImage: `url(${GossipImg})`, backgroundSize: "center", minHeight: "100vh"}} */}
+        < div  >
+        </div>
 
         </div>
         
