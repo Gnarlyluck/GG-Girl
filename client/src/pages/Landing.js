20e5077e82adaa5320c12bf148893063df1dfcec
Gnarlyluck <schuylerluckey@gmail.com>
2020-11-16 16:00:31 -0800

    removed nav from landing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import GossipImg from '../img-file/gg-background.jpg'
-import Nav from '../components/Nav'
 
 
 // style={{backgroundImage: `url(${GossipImg})`, backgroundSize: "cover", minHeight: "100vh"}}
@@ -8,7 +7,6 @@ import Nav from '../components/Nav'
 const Home = (props) => {
   return (
   <div>
-    <Nav />
         <p>XOXO GOSSIP GIRL</p>
         <div className="img-wrapper">
         {/* <img src={GossipImg} alt="NY skyline with XOXO" /> */}
